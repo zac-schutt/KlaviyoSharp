06d7dd11424be1488710032761952b045079c040
Zac Schutt <48711922+zac-schutt@users.noreply.github.com>
2023-07-17 13:22:17 +1000

    Refactor AccountServices Tests

--- KlaviyoSharp.Tests/KlaviyoTests/AccountServices_Tests.cs
+++ KlaviyoSharp.Tests/KlaviyoTests/AccountServices_Tests.cs
@@ -11,39 +11,16 @@ public class AccountServices_Tests : IClassFixture<AccountServices_Tests_Fixture
     [Fact]
     public async Task GetAccounts()
     {
-        var accounts = await Fixture.AdminApi.AccountServices.GetAccounts();
-        Fixture.AccountId ??= accounts.Data[0].Id;
-        Assert.Single(accounts.Data);
-    }
-    [Fact]
-    public async Task GetAccountsWithFields()
-    {
         var accounts = await Fixture.AdminApi.AccountServices.GetAccounts(new List<string>() { "public_api_key" });
-        Fixture.AccountId ??= accounts.Data[0].Id;
         Assert.Single(accounts.Data);
-    }
-
-    [Fact]
-    public async Task GetAccount()
-    {
-        Fixture.AccountId ??= Fixture.AdminApi.AccountServices.GetAccounts().Result.Data[0].Id;
-        var account = await Fixture.AdminApi.AccountServices.GetAccount(Fixture.AccountId);
-        Assert.NotNull(account);
-    }
-
-    [Fact]
-    public async Task GetAccountWithFields()
-    {
-        Fixture.AccountId ??= Fixture.AdminApi.AccountServices.GetAccounts().Result.Data[0].Id;
-        var account = await Fixture.AdminApi.AccountServices.GetAccount(Fixture.AccountId, new List<string>() { "public_api_key","contact_information.street_address" });
-        Assert.NotNull(account);
+        var account = await Fixture.AdminApi.AccountServices.GetAccount(accounts.Data[0].Id, new List<string>() { "public_api_key", "contact_information.street_address" });
+        Assert.NotNull(account.Data);
     }
 
 }
 public class AccountServices_Tests_Fixture : IAsyncLifetime
 {
-    public KlaviyoAdminApi AdminApi { get; } = new(Config.ApiKey);
-    public string AccountId = null;
+    public KlaviyoAdminApi AdminApi { get; } = new(new KlaviyoConfig(Config.ApiKey) { MaxDelay = 120, MaxRetries = 5 });
     public Task DisposeAsync()
     {
         return Task.CompletedTask;
